63ff5523d858ebacfb9d5ab653671ae9edcd0102
simonebonifazi <simonbonifraz@gmail.com>
2022-09-01 09:08:37 +0200

    imported fontawesome

--- index.php
+++ index.php
@@ -46,3 +46,16 @@ $faqs = [
     ],
 ];
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.css' integrity='sha512-HHsOC+h3najWR7OKiGZtfhFIEzg5VRIPde0kB0bG2QRidTQqf+sbfcxCTB16AcFB93xMjnBIKE29/MjdzXE+qw==' crossorigin='anonymous'/>
+    <title>Google's FAQS</title>
+</head>
+<body>
+    
+</body>
+</html>
